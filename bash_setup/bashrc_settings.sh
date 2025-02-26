1143a163ee66ecb9ddc58215ac4cdd568048af75
feifei-111 <2364819892@qq.com>
2025-02-26 15:22:04 +0800

    update

--- bash_setup/bashrc_settings.sh
+++ bash_setup/bashrc_settings.sh
@@ -11,6 +11,8 @@ alias showG='env | grep CUDA_VISIBLE_DEVICES'
 alias setG='GPU_script(){ export CUDA_VISIBLE_DEVICES=$1;}; GPU_script'
 alias ps1='SetPS1(){ source ~/bash_setup_scripts/set_ps1.sh;}; SetPS1'
 alias apyp='AddPYPATH(){ export PYTHONPATH=$PWD:$PYTHONPATH;}; AddPYPATH'
+alias gitlocal='ZZ(){ git config --global user.name "feifei-111"; git config --global user.email "2364819892@qq.com";}; ZZ'
+alias gitglobal='ZZZ(){ git config --global user.name "feifei-111"; git config --global user.email "2364819892@qq.com";}; ZZZ'
 
 unset GREP_OPTIONS
 ps1
